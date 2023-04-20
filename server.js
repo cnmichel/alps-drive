af23b77eb46bc4023086a53fbf051be5166cf498
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-04-20 09:08:12 +0200

    task: init project with express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use('/', express.static('frontend'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app;
